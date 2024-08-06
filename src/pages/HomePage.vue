bc817b4e5325e0f781fd2a3427f051c7df5e50f3
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-08-06 15:30:18 +0530

    finished the homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,43 +1,39 @@
 <template>
-    <div class="homePage-container">
-        <div class="homePage-title-container">
-            <div class="homePage-title">
-                <span>
-                    Providing Top-tier
-                </span>
-                <span id="greyTitle">
-                    legal services.
-                </span>
-                <span id="subtitle">
-                    Bravery courage fear and love in times of war
-                </span>
-                <div class="contact-container">
-                    <span>
-                        <i class="bi bi-telephone-fill"></i>
-                        <p>Call us : 0123456789</p>
-                    </span>
-                    <span>
-                        <i class="bi bi-envelope-fill"></i>
-                        <p>email : someone@gmail.com</p>
-                    </span>
-                </div>
-            </div>
+  <div class="homePage-container">
+    <div class="homePage-title-container">
+      <div class="homePage-title">
+        <span> Providing top-tier </span>
+        <span id="greyTitle"> legal services. </span>
+        <span id="subtitle">
+          Bravery courage fear and love in times of war
+        </span>
+        <div class="contact-container">
+          <span>
+            <i class="bi bi-telephone-fill"></i>
+            <p>Call us : 0123456789</p>
+          </span>
+          <span>
+            <i class="bi bi-envelope-fill"></i>
+            <p>email : someone@gmail.com</p>
+          </span>
         </div>
-        <div class="homepage-image-container">
-
-        </div>
+      </div>
     </div>
+    <div class="homepage-image-container">
+        <img :src="homeImage" alt="">
+    </div>
+  </div>
 </template>
 
 <script>
-import '../style/homepage.css'
-import homeImage from '../assets/homepageimage.jpg'
+import "../style/homepage.css";
+import homeImage from "../assets/black-white-portrait-digital-nomads.jpg";
 export default {
-    name:'HomePage',
-    data(){
-        return{
-            homeImage:homeImage
-        }
-    }
-}
+  name: "HomePage",
+  data() {
+    return {
+      homeImage: homeImage,
+    };
+  },
+};
 </script>
