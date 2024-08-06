974ed5d1ebd645949b7edb4b824525f226382b5d
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-08-06 19:14:01 +0530

    added the gsap animations to homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,17 +2,17 @@
   <div class="homePage-container">
     <div class="homePage-title-container">
       <div class="homePage-title">
-        <span> Providing top-tier </span>
-        <span id="greyTitle"> legal services. </span>
-        <span id="subtitle">
+        <span class="title">Providing top-tier</span>
+        <span id="greyTitle" class="title">legal services.</span>
+        <span id="subtitle" >
           Bravery courage fear and love in times of war
         </span>
         <div class="contact-container">
-          <span>
+          <span class="contact-info">
             <i class="bi bi-telephone-fill"></i>
             <p>Call us : 0123456789</p>
           </span>
-          <span>
+          <span class="contact-info">
             <i class="bi bi-envelope-fill"></i>
             <p>email : someone@gmail.com</p>
           </span>
@@ -20,7 +20,7 @@
       </div>
     </div>
     <div class="homepage-image-container">
-        <img :src="homeImage" alt="">
+      <img :src="homeImage" alt="Portrait of Digital Nomads" />
     </div>
   </div>
 </template>
@@ -28,12 +28,46 @@
 <script>
 import "../style/homepage.css";
 import homeImage from "../assets/black-white-portrait-digital-nomads.jpg";
+import {gsap} from "gsap"
 export default {
+
   name: "HomePage",
   data() {
     return {
       homeImage: homeImage,
     };
   },
+  mounted() {
+    gsap.from('.title',{
+      opacity:0,
+      duration:1,
+      stagger:0.3,
+      y:50,
+      ease:'power3.inOut',
+    })
+    gsap.from('#subtitle',{
+      delay:0.4,
+      duration:1,
+      opacity:0,
+      y:50,
+      ease:'power3.inOut'
+    })
+    gsap.from('.contact-info',{
+      y:50,
+      stagger:0.3,
+      duration:1,
+      ease:'power3.inOut',
+      opacity:0,
+      delay:0.6
+    })
+    gsap.from('.homepage-image-container img',{
+      duration:1,
+      delay:0.7,
+      opacity:0,
+      x:100,
+      ease:'power3.inOut'
+    })
+  }
 };
 </script>
+
