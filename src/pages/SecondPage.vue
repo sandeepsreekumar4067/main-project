982eea1724ec4cca3413f4c398e12a61de3bcfe2
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-08-06 22:42:24 +0530

    made the second container

--- src/pages/SecondPage.vue
+++ src/pages/SecondPage.vue
@@ -31,7 +31,7 @@
       <span class="animated-span"> professional excellence</span>
     </div>
     <div class="secondPage-third-container">
-      <div class="secondContainer-title">Representing Cliens in</div>
+      <div class="secondContainer-title">Representing Clients in</div>
       <div class="third-container-buttons">
         <span>Corporate Law</span>
         <span>Criminal Law</span>
@@ -43,52 +43,98 @@
 
 <script>
 import "../style/secondpage.css";
-import {gsap} from 'gsap'
+import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 export default {
-    name:'SecondPage',
-    data(){
-        return{
-
-        }
-    },
-    mounted(){
-        gsap.from('.animated-container-1',{
-            scrollTrigger:{
-                trigger:'.animated-container-1',
-                start:'top 85%',
-                toggleActions : 'play none none none'
-            },
-            opacity:0,
-            duration:0.7,
-            stagger:0.3,
-            x:-100
-        })
-        gsap.from('.animated-container-2',{
-            scrollTrigger:{
-                trigger:'animated-container-2',
-                start:'top 85%',
-                toggleActions:'play none none none'
-            },
-            opacity:0,
-            duration:1,
-            delay:0.4,
-            x:-100,
-            stagger:0.4
-        })
-        gsap.from('.animated-container-3',{
-            scrollTrigger:{
-                trigger:'.animated-trigger-3',
-                start:'top 85%',
-                toggleActions:'play none none none'
-            },
-            opacity:0,
-            duration:1.1,
-            delay:0.6,
-            x:-100,
-            stagger:0.6
-        })
-    }
+  name: "SecondPage",
+  data() {
+    return {};
+  },
+  mounted() {
+    gsap.from(".animated-container-1", {
+      scrollTrigger: {
+        trigger: ".animated-container-1",
+        start: "top 85%",
+        toggleActions: "play none none none",
+      },
+      opacity: 0,
+      duration: 0.7,
+      stagger: 0.3,
+      x: -100,
+    });
+    gsap.from(".animated-container-2", {
+      scrollTrigger: {
+        trigger: ".animated-container-2",
+        start: "top 80%",
+        toggleActions: "play none none none",
+      },
+      opacity: 0,
+      duration: 1,
+      delay: 0.4,
+      x: -100,
+      stagger: 0.4,
+    });
+    gsap.from(".animated-container-3", {
+      scrollTrigger: {
+        trigger: ".animated-container-3",
+        start: "top 80%",
+        toggleActions: "play none none none",
+      },
+      opacity: 0,
+      duration: 1.1,
+      delay: 0.6,
+      x: -100,
+      stagger: 0.6,
+    });
+    gsap.from(".secondPage-second-container", {
+      scrollTrigger: {
+        trigger: ".secondPage-second-container",
+        start: "top 80%", // Start animation when element reaches 80% of the viewport
+        toggleActions: "play none none none",
+      },
+      delay: 0.2,
+      opacity: 0,
+      x: -100,
+      duration: 1,
+      ease: "power3.out",
+    });
+    gsap.from(".animated-span", {
+      scrollTrigger: {
+        trigger: ".animated-span",
+        start: "top 85%",
+        toggleActions: "play none none none",
+      },
+      opacity: 0,
+      x: -100,
+      duration: 1,
+      stagger: 0.3,
+      delay: 0.4,
+    });
+    gsap.from('.secondContainer-title',{
+        scrollTrigger:{
+            trigger:'.secondContainer-title',
+            start:'top 90%',
+            toggleActions:'play none none none'
+        },
+        opacity:0,
+        x:-100,
+        duration:1,
+        delay:0.3,
+        stagger:0.3
+    })
+    gsap.from('.third-container-buttons span',{
+        scrollTrigger:{
+            trigger:'.third-container-buttons',
+            start:'top 90%',
+            toggleActions:'play none none none'
+        },
+        opacity:0,
+        duration:1,
+        stagger:0.4,
+        delay:0.5,
+        x:-100
+    })
+  },
 };
 </script>
