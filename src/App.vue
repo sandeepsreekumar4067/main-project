71cf56cbe7e23973b39036994a8eabc4faacef7c
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-08-10 23:25:25 +0530

    router has been made

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <NavBar/>
-  <HomeRender/>
+  <router-view/>
 </template>
 
 <script>
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../src/style/app.css'
 import NavBar from './components/NavBar.vue';
-import HomeRender from './pages/HomeRender.vue';
 
 export default {
   name: 'App',
   components: {
     NavBar,
-    HomeRender
   }
 }
 </script>
