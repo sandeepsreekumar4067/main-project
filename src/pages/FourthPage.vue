5063d0700b4dfae2958ceb90aea14438bc0347a6
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-08-12 19:21:24 +0530

    made the circle model , so far no bug all good

--- src/pages/FourthPage.vue
+++ src/pages/FourthPage.vue
@@ -3,10 +3,18 @@
     <div class="fourth-page-components" id="comp">
       <div class="fourthPage-main-title">Our Services</div>
       <div class="fourthPage-circle-container">
-        <span class="service-span s1"></span>
-        <span class="service-span s2"></span>
-        <span class="service-span s3"></span>
-        <span class="service-span s4"></span>
+        <span class="service-span s1">
+          <div class="circular-text">Service 1</div>
+        </span>
+        <span class="service-span s2">
+          <div class="circular-text">Service 2</div>
+        </span>
+        <span class="service-span s3">
+          <div class="circular-text">Service 3</div>
+        </span>
+        <span class="service-span s4">
+          <div class="circular-text">Service 4</div>
+        </span>
       </div>
     </div>
   </div>
@@ -33,12 +41,18 @@ export default {
       borderRadius: "50%",
       xPercent: 100,
     });
-    timeline.fromTo(".fourthPage-main-title", { x: 0, opacity: 0 }, {
-        xPercent:-70,
-        opacity:1,
-    },
-    '+=0.6'
-);
+    timeline.fromTo(
+      ".fourthPage-main-title",
+      { x: 0, opacity: 0 },
+      {
+        xPercent: -70,
+        opacity: 1,
+      },
+      "+=0.6"
+    );
+    timeline.to(".service-span", {
+      opacity: 1,
+    });
   },
 };
 </script>
