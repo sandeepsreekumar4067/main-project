738a1cf1ead225ae0aaf926dfc43e273c01930a0
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-08-12 18:43:06 +0530

    made teh fourth page / re making evreything

---
+++ src/pages/FourthPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="fourth-page-container">
+
+  </div>
+</template>
+
+<script>
+import '../style/fourthpage.css'
+export default {
+    name:'FourthPage'
+}
+</script>
+
+<style>
+
+</style>
