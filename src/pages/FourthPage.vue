cb87fcb1f2536bbfccab082343277d9805da4869
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-08-12 19:12:47 +0530

    so far so good

--- src/pages/FourthPage.vue
+++ src/pages/FourthPage.vue
@@ -1,43 +1,47 @@
 <template>
   <div class="fourth-page-container" id="main">
     <div class="fourth-page-components" id="comp">
-        <div class="forthPage-main-title">
-
-        </div>
-        <div class="fourthPage-circle-container">
-
-        </div>
+      <div class="fourthPage-main-title">Our Services</div>
+      <div class="fourthPage-circle-container">
+        <span class="service-span s1"></span>
+        <span class="service-span s2"></span>
+        <span class="service-span s3"></span>
+        <span class="service-span s4"></span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import '../style/fourthpage.css'
-import {gsap} from 'gsap'
+import "../style/fourthpage.css";
+import { gsap } from "gsap";
 export default {
-    name:'FourthPage',
-    mounted(){
-        const timeline = gsap.timeline({
-            scrollTrigger:{
-                scrub:1,
-                trigger:'#comp',
-                start:'top 20%',
-                endTrigger:'#main',
-                end:'bottom bottom',
-                markers:true,
-                pin:true
-            }
-        })
-        timeline.to('.fourthPage-circle-container',{
-            rotate:360,
-            borderRadius:'50%',
-            xPercent:100
-        })
-    }
-
-}
+  name: "FourthPage",
+  mounted() {
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        scrub: 1,
+        trigger: "#comp",
+        start: "top 20%",
+        endTrigger: "#main",
+        end: "bottom bottom",
+        pin: true,
+      },
+    });
+    timeline.to(".fourthPage-circle-container", {
+      rotate: 360,
+      borderRadius: "50%",
+      xPercent: 100,
+    });
+    timeline.fromTo(".fourthPage-main-title", { x: 0, opacity: 0 }, {
+        xPercent:-70,
+        opacity:1,
+    },
+    '+=0.6'
+);
+  },
+};
 </script>
 
 <style>
-
 </style>
