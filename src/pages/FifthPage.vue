18d87a208f7e141e313820fe20bea30b01c04b72
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-08-16 01:16:32 +0530

    making the spokes

--- src/pages/FifthPage.vue
+++ src/pages/FifthPage.vue
@@ -5,9 +5,20 @@
         <div class="pseudo-circle"></div>
         <span class="s5" id="s15"></span>
         <span class="s5" id="s25"></span>
-        <span class="s5" id="s35"></span>
+        <span class="s5" id="s35"> </span>
         <span class="s5" id="s45"></span>
-        <span class="s5" id="s55"></span>
+        <span class="s5" id="s55">
+          <div class="pseudoSpan">
+            <div class="spoke spoke-1"></div>
+            <div class="spoke spoke-2"></div>
+            <div class="spoke spoke-3"></div>
+            <div class="spoke spoke-4"></div>
+            <div class="spoke spoke-5"></div>
+            <div class="spoke spoke-6"></div>
+            <div class="spoke spoke-7"></div>
+            <div class="spoke spoke-8"></div>
+          </div>
+        </span>
         <span class="s5" id="s65"></span>
         <span class="s5" id="s75"></span>
         <span class="s5" id="s85"></span>
@@ -25,97 +36,96 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default {
   name: "FifthPage",
-    mounted() {
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: ".fifthpage-object-container",
-          scrub: 1,
-          start: "top 30%",
-          pin: true,
-          markers: true,
-          endTrigger: ".fifth-page-container",
-          end: "bottom bottom",
-        },
-      });
+  mounted() {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".fifthpage-object-container",
+        scrub: 1,
+        start: "top 30%",
+        pin: true,
+        markers: true,
+        endTrigger: ".fifth-page-container",
+        end: "bottom bottom",
+      },
+    });
 
-      tl.from(".fifthpage-object", {
-        xPercent: 500,
-        duration: 1,
-        rotate: 360,
-      });
-      tl.to('#s15',{
-        duration:0.8,
-        xPercent:-300,
-        yPercent:-60,
-        width:'100%',
-        clipPath:'none',
-        borderRadius:'20px'
-      })
-      tl.to('#s25',{
-        xPercent:-180,
-        rotate:0,
-        yPercent:-60,
-        width:'100%',
-        clipPath:'none',
-        borderRadius:'20px'
-      })
-      tl.to('#s35',{
-        xPercent:-55,
-        rotate:0,
-        yPercent:-60,
-        width:'100%',
-        clipPath:'none',
-        borderRadius:'20px',
-      })
-      tl.to('#s45',{
-        xPercent:-300,
-        rotate:0,
-        yPercent:50,
-        width:'100%',
-        clipPath:'none',
-        borderRadius:'20px'
-      })
-      tl.to('#s55',{
-        xPercent:-180,
-        rotate:0,
-        yPercent:50,
-        width:'100%',
-        clipPath:'none',
-        borderRadius:'20px',
-        backgroundColor:'transparent'
-
-      })
-      tl.to('#s65',{
-        xPercent:-55,
-        rotate:0,
-        yPercent:50,
-        width:'100%',
-        clipPath:'none',
-        borderRadius:'20px'
-      })
-      tl.to('#s75',{
-        xPercent:-300,
-        rotate:0,
-        yPercent:160,
-        width:'100%',
-        clipPath:'none',
-        borderRadius:'20px'
-      })
-      tl.to('#s85',{
-        xPercent:-180,
-        rotate:0,
-        yPercent:160,
-        width:'100%',
-        clipPath:'none',
-        borderRadius:'20px'
-      })
-      tl.to('.pseudo-circle',{
-        borderRadius:'20px',
-        xPercent:-5,
-        yPercent:160,
-        height:'50%',
-        
-    })
-    },
+    tl.from(".fifthpage-object", {
+      xPercent: 500,
+      duration: 1,
+      rotate: 360,
+    });
+    tl.to("#s15", {
+      duration: 0.8,
+      xPercent: -300,
+      yPercent: -60,
+      width: "100%",
+      clipPath: "none",
+      borderRadius: "20px",
+    });
+    tl.to("#s25", {
+      xPercent: -180,
+      rotate: 0,
+      yPercent: -60,
+      width: "100%",
+      clipPath: "none",
+      borderRadius: "20px",
+    });
+    tl.to("#s35", {
+      xPercent: -55,
+      rotate: 0,
+      yPercent: -60,
+      width: "100%",
+      clipPath: "none",
+      borderRadius: "20px",
+    });
+    tl.to("#s45", {
+      xPercent: -300,
+      rotate: 0,
+      yPercent: 50,
+      width: "100%",
+      clipPath: "none",
+      borderRadius: "20px",
+    });
+    tl.to("#s55", {
+      xPercent: -180,
+      rotate: 0,
+      yPercent: 50,
+      width: "100%",
+      clipPath: "none",
+      borderRadius: "20px",
+      duration: 1,
+      backgroundColor: "transparent",
+    });
+    tl.to("#s65", {
+      xPercent: -55,
+      rotate: 0,
+      yPercent: 50,
+      width: "100%",
+      clipPath: "none",
+      borderRadius: "20px",
+    });
+    tl.to("#s75", {
+      xPercent: -300,
+      rotate: 0,
+      yPercent: 160,
+      width: "100%",
+      clipPath: "none",
+      borderRadius: "20px",
+    });
+    tl.to("#s85", {
+      xPercent: -180,
+      rotate: 0,
+      yPercent: 160,
+      width: "100%",
+      clipPath: "none",
+      borderRadius: "20px",
+    });
+    tl.to(".pseudo-circle", {
+      borderRadius: "20px",
+      xPercent: -5,
+      yPercent: 160,
+      height: "50%",
+    });
+  },
 };
 </script>
