dc315d3c5c2efe0f42516e4d9c2496e90b1a3662
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-08-16 00:07:46 +0530

    about to give animations to the new pie

--- src/pages/FifthPage.vue
+++ src/pages/FifthPage.vue
@@ -43,69 +43,7 @@ export default {
         rotate: 360,
       });
 
-      // Burst effect at the end
-      tl.to(
-        "#s15",
-        {
-          borderTopLeftRadius: "100%",
-        },
-        "-=0.5"
-      ).to("#s15", {
-        xPercent: -70, // Move left
-        yPercent: -50, // Move up
-        duration: 1,
-      }).to('#s15',{
-          borderTopLeftRadius: "0%",
-
-      },'-=0.75').to('#s15',{
-        width:'100%',
-      })
-      tl.to(
-        "#s25",
-        {
-          borderTopRightRadius: "100%",
-        },
-        "-=2"
-      ).to("#s25", {
-        xPercent: 70, // Move right
-        yPercent: -50, // Move up
-        duration: 1,
-      }).to('#s25',{
-          borderTopRightRadius: "0%",
-
-      },'-=1').to('#s25',{width:'100%'})
-      tl.to(
-        "#s35",
-        {
-          borderBottomLeftRadius: "100%",
-        },
-        "-=3.5"
-      ).to("#s35", {
-        xPercent: -70, // Move left
-        yPercent: 50, // Move down
-        duration: 1,
-      }).to('#s35',{
-          borderBottomLeftRadius: "0%",
-
-      },'-=1.25').to('#s35',{width:'100%'})
-      tl.to(
-        "#s45",
-        {
-          borderBottomRightRadius: "100%",
-        },
-        "-=5"
-      ).to("#s45", {
-        xPercent: 70, // Move left
-        yPercent: 50, // Move down
-        duration: 1,
-      }).to('#s45',{
-          borderBottomRightRadius: "0%",
-
-      },'-=1.25').to('#s45',{width:'100%'})
-      tl.to('.fifthpage-object-title',{
-          opacity:1,
-          duration:0.8
-      })
+    
     },
 };
 </script>
