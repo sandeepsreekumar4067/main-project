41677f2262078002855bb18e786d4b3a6292f4ac
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-08-16 02:53:14 +0530

    improved the second page

--- src/pages/SecondPage.vue
+++ src/pages/SecondPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="secondPage-container">
     <div class="secondPage-first-container">
-      <span class="animated-container-1">
+      <span class="animated-container-1 spa">
         Advocacy
         <p>
           "Empowering clients through expert advocacy, we combine legal
@@ -9,7 +9,7 @@
           rights protected."
         </p>
       </span>
-      <span class="animated-container-2">
+      <span class="animated-container-2 spa">
         Consultation
         <p>
           "Access expert legal consultation with ease. Schedule personalized
@@ -17,7 +17,7 @@
           needs."
         </p>
       </span>
-      <span class="animated-container-3">
+      <span class="animated-container-3 spa">
         Expertise
         <p>
           "Leverage our unparalleled expertise, combining legal proficiency and
@@ -44,98 +44,69 @@
 <script>
 import "../style/secondpage.css";
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
 export default {
   name: "SecondPage",
   data() {
     return {};
   },
   mounted() {
-    gsap.from(".animated-container-1", {
-      scrollTrigger: {
-        trigger: ".animated-container-1",
-        start: "top 85%",
-        toggleActions: "play pause resume reverse",
+    gsap.from('.spa',{
+      scrollTrigger:{
+        trigger:'.secondPage-first-container',
+        start:'top 70%',
+        toggleActions:'play pause resume reverse'
       },
-      opacity: 0,
-      duration: 0.7,
-      stagger: 0.3,
-      x: -100,
-    });
-    gsap.from(".animated-container-2", {
-      scrollTrigger: {
-        trigger: ".animated-container-2",
-        start: "top 80%",
-        toggleActions: "play pause resume reverse",
+      xPercent:-100,
+      opacity:0,
+      stagger:0.4,
+      duration:1.5,
+    })
+    gsap.from('.secondPage-second-container',{
+      scrollTrigger:{
+        trigger:'.secondPage-second-container',
+        start:'top 70%',
+        toggleActions:'play pause resume reverse'
       },
-      opacity: 0,
-      duration: 1,
-      delay: 0.4,
-      x: -100,
-      stagger: 0.4,
-    });
-    gsap.from(".animated-container-3", {
-      scrollTrigger: {
-        trigger: ".animated-container-3",
-        start: "top 80%",
-        toggleActions: "play pause resume reverse",
+      xPercent:-100,
+      opacity:0,
+      stagger:0.4,
+      duration:1.5,
+    }
+    )
+    gsap.from('.animated-span',{
+      scrollTrigger:{
+        trigger:'.animated-span',
+        start:'top 60%',
+        toggleActions:'play pause resume reverse'
       },
-      opacity: 0,
-      duration: 1.1,
-      delay: 0.6,
-      x: -100,
-      stagger: 0.6,
-    });
-    gsap.from(".secondPage-second-container", {
-      scrollTrigger: {
-        trigger: ".secondPage-second-container",
-        start: "top 80%", // Start animation when element reaches 80% of the viewport
-        toggleActions: "play pause resume reverse",
-      },
-      delay: 0.2,
-      opacity: 0,
-      x: -100,
-      duration: 1,
-      ease: "power3.out",
-    });
-    gsap.from(".animated-span", {
-      scrollTrigger: {
-        trigger: ".animated-span",
-        start: "top 85%",
-        toggleActions: "play pause resume reverse",
-      },
-      opacity: 0,
-      x: -100,
-      duration: 1,
-      stagger: 0.3,
-      delay: 0.4,
-    });
+      xPercent:-100,
+      opacity:0,
+      duration:1.5,
+    }
+    )
     gsap.from('.secondContainer-title',{
-        scrollTrigger:{
-            trigger:'.secondContainer-title',
-            start:'top 95%',
-            toggleActions:"play pause resume reverse"
-        },
-        opacity:0,
-        x:-100,
-        duration:1,
-        delay:0.3,
-        stagger:0.3
+      scrollTrigger:{
+        trigger:'.secondPage-third-container',
+        start:'top 70%',
+        toggleActions:'play pause resume reverse'
+      },
+      delay:0.4,
+      xPercent:-100,
+      opacity:0,
+      duration:1.5,
     })
-    gsap.from('.third-container-buttons span',{
-        scrollTrigger:{
-            trigger:'.third-container-buttons',
-            start:'top 70%',
-            toggleActions:"play pause resume reverse"
-        },
-        opacity:0,
-        duration:1,
-        stagger:0.3,
-        delay:0.5,
-        x:-100
+    gsap.from('.third-container-buttons',{
+      scrollTrigger:{
+        trigger:'.secondPage-third-container',
+        start:'top 70%',
+        toggleActions:'play pause resume reverse'
+      },
+      delay:0.7,
+      xPercent:-100,
+      stagger:0.4,
+      opacity:0,
+      duration:1.5,
     })
-    ScrollTrigger.refresh();
   },
 };
 </script>
